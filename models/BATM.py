cc12e8ef1b1bb7a2df68780c970cac909d725018
tlhdan <95524572+tlhdan@users.noreply.github.com>
2025-06-06 22:40:52 +0700

    Fix ModuleNotFoundError: No module named 'gan'

--- models/BATM.py
+++ models/BATM.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from .gan import Generator, Encoder, Discriminator
+from gan import Generator, Encoder, Discriminator
 import sys
 sys.path.append('..')
 from utils import evaluate_topic_quality, smooth_curve
